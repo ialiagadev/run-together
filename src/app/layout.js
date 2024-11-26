30a212e201f2157438d8bd9ccfe9c083ad4b4fb1
ivan <ialiagadev@gmail.com>
2024-11-26 11:55:23 +0100

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { AuthProvider } from './contexts/AuthContext'
-import Navbar from './components/Navbar'
 import ClientLayout from './components/ClientLayout'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,12 +8,14 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'SocialKM',
   description: 'Encuentra compañeros para correr en tu área',
+  icons: {
+    icon: '/favicon.ico', // Ruta al favicon en la carpeta public
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es" className={inter.className}>
-      <head><link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" /></head>
       <body className="bg-gradient-to-br from-purple-900/50 to-black text-white min-h-screen flex flex-col">
         <AuthProvider>
           <ClientLayout>
